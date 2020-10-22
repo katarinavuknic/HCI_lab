6a70427be1b493832656dfd60797ba8de4f78777
katarinavuknic <katarinavuknic12345@gmail.com>
2020-10-22 12:46:22 +0200

    adding Navigation

---
+++ moj-projekt/src/components/NavigationBar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles from './style.module.css'
+
+const NavigationBar = () => (
+  <nav className={styles.navigationBar}>
+      <li className={styles.active}>Home</li>
+      <li>Accommodation</li>
+      <li>Photo gallery</li>
+      <li>Contact</li>
+  </nav>
+)
+
+export default NavigationBar
